6973f30f1bd3622d4f9314b1dae19f0f2890cbf0
hwangithub <whgkim@hotmail.com>
2023-05-15 20:09:55 -0700

    databases, name edit

--- Jinstagram/settings.py
+++ Jinstagram/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': 'db-devops-restart-instance-1.c0yz9ahgb9ne.us-west-1.rds.amazonaws.com',
-        'NAME': 'youtube_devops',
+        'NAME': 'devops_restart',
         'USER': 'admin',
         'PASSWORD': 'hwan12345',
         'PORT': '3306',
